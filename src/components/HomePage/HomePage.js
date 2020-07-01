b0dd326770066bb098abb96e342644300173bf2d
Yurii <yurdosii.ksv@gmail.com>
2020-07-01 19:53:40 +0300

    Added routers and components

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="App">
+            Home page
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div>
+            Login page
+        </div>
+    );
+}
+
+export default LoginPage;
