97f57e6eb1f6e716d4f7a7d3ac8be5067e7c784b
Yurii <yurdosii.ksv@gmail.com>
2020-07-22 11:47:05 +0300

    Removed Header component

--- src/routers.js
+++ src/routers.js
@@ -4,14 +4,12 @@ import { Switch, Route } from 'react-router';
 
 import LoginPage from './components/LoginPage/LoginPage';
 import HomePage from './components/HomePage/HomePage';
-import Header from './components/Header/Header';
 
 
 class Routers extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Header />
                 <Switch>
                     <Route path="/login"
                         component={LoginPage} />
