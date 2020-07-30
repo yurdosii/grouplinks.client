66eb9ce5b69f039f6061cf2a5821e56ddfba27bc
Yurii <yurdosii.ksv@gmail.com>
2020-07-30 20:06:01 +0300

    Updated constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,9 @@
-export const SERVER_HOST = 'http://127.0.0.1:8000';
+// export const SERVER_HOST = 'http://127.0.0.1:8000';
+export const SERVER_HOST = 'http://grouplinksm.com:8000';
 export const API_URL = `${SERVER_HOST}/api`;
 export const API_VERSION = 'v1';
+
+export const CLIENT_HOST = 'http://grouplinksm.com:3000';
+export const MAIN_PAGE_URL = `${CLIENT_HOST}/`;
+
+export const GOOGLE_AUTH_URL =  `${SERVER_HOST}/social/login/google-oauth2`
